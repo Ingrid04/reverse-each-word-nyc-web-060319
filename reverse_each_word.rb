fc496a3e9cadd435f2d1e9eddf971e30f5c98b56
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-29 21:20:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word
-  sentence1 = "Hello there, and how are you?"
-  sentence1.map {|string| string.reverse.join}.join(" ")
+  sentence1 = string
+  original_array = string.split ("Hello there, and how are you?")
+  return_array = []
+  sentence1.each do |string|
+    return_array << string.reverse
   end 
+  return_array.join("string")
 end
-
