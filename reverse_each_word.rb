cb834ea8e924ce9812a3b44d2fc581c10694c18c
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-29 21:02:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word
   sentence1 = "Hello there, and how are you?"
-  sentence1.each do |sentence1.reverse|
-    return sentence1
+  sentence1.each do |sentence1|
+    return return_array << string.reverse
     
 end
