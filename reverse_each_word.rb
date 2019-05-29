69be5698025c1961318e9c631f5b8bc23aafda31
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-29 21:14:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,6 @@ def reverse_each_word
   sentence1 = "Hello there, and how are you?"
   sentence1.map {|string| string.reverse.join}.join(" ")
   end 
+    puts sentence1.map {|string| string.reverse.join}.join(" ")
 end
 
